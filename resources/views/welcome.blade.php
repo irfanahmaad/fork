a2b7cf260c6d2d0d3c7a6bb16778d4d7ac33f9ed
zerocryptor <zerocryptor@users.noreply.github.com>
2019-05-17 09:48:21 +0700

    add dimas after blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,10 +89,10 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
+                    <a href="#">Dimas</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <a href="#">Master</a>
                 </div>
             </div>
         </div>
